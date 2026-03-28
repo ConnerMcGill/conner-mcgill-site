ed96316eececdaaa59419a097e2bb4d25a554bce
ConnerMcGill <connermcgill9@gmail.com>
2026-03-28 22:33:29 +0000

    Tweak App.tsx more

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
       <div>
         <h1>Hello World!</h1>
         <a href="https://github.com/ConnerMcGill/">Conner McGill GitHub Profile</a>
+        <a href="https://github.com/ConnerMcGill/conner-mcgill-site">Conner McGill Dev/Blog Site Repository</a>
       </div>
   );
 }
